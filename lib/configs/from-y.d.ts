417e21a64cceb95686b67b85610d2ebca6b10695
a.ladan <arvinall021@gmail.com>
2022-07-24 18:49:13 +0430

    docs(configs): add fromY/from config documentation and declaration

---
+++ lib/configs/from-y.d.ts
@@ -0,0 +1,17 @@
+import { IUnitValue } from '../units'
+
+/**
+ * @example
+ * ```js
+ * const viewPortSize = percent(() => window.innerHeight)
+ * 
+ * fromY(viewPortSize(150)) // -> { fromY: viewPortSize(150), from: viewPortSize(150) }
+ * 
+ * fromY(500) // -> { fromY: px(500), from: px(500) }
+ * ```
+ * 
+ * @returns
+ * an object with `fromY` and `from` properties
+ * that both of them are equal to `value` argument
+ */
+export function fromY (value: IUnitValue): { fromY: IUnitValue, from: IUnitValue }
