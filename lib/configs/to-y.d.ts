33a9a9a376335697b9684f3030380d89346f3130
a.ladan <arvinall021@gmail.com>
2022-07-27 12:23:01 +0430

    docs(configs): add toY config documentation and declaration

---
+++ lib/configs/to-y.d.ts
@@ -0,0 +1,17 @@
+import { IUnitValue } from '../units'
+
+/**
+ * @example
+ * ```js
+ * const viewPortSize = percent(() => window.innerHeight)
+ * 
+ * toY(viewPortSize(150)) // -> { toY: viewPortSize(150), to: viewPortSize(150) }
+ * 
+ * toY(500) // -> { toY: px(500), to: px(500) }
+ * ```
+ * 
+ * @returns
+ * an object with `toY` and `to` properties
+ * that both of them are equal to `value` argument
+ */
+export function toY (value: IUnitValue): { toY: IUnitValue, to: IUnitValue }
