e7d882658e043e8b5635935cd74d89c0ff665bdb
a.ladan <arvinall021@gmail.com>
2023-01-07 14:03:25 +0330

    style(units): remove extra break lines from IUnitValue and IUnit

--- lib/units/index.d.ts
+++ lib/units/index.d.ts
@@ -1,12 +1,8 @@
 /** UnitValue is a function that returns a number */
-export interface IUnitValue {
-  (): number
-}
+export interface IUnitValue { (): number }
 
 /** Unit is a function that returns a {@link IUnitValue} */
-export interface IUnit {
-  (...args: unknown[]): IUnitValue
-}
+export interface IUnit { (...args: unknown[]): IUnitValue }
 
 /** UnitInput is a number or {@link IUnitValue} */
 export type IUnitInput = number | IUnitValue
