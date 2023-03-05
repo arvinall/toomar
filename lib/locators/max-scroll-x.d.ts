4d581edfc4549bf4ab92690b99481d359414ee8e
a.ladan <arvinall021@gmail.com>
2023-03-05 18:17:54 +0330

    docs(locators): add example to max-scroll-x documentation

--- lib/locators/max-scroll-x.d.ts
+++ lib/locators/max-scroll-x.d.ts
@@ -3,6 +3,19 @@ import { IUnitValue } from '../units'
 /**
  * @param source default value is `globalThis`
  * 
+ * @example
+ * ```js
+ * const documentMaxScrollX = maxScrollX()
+ * 
+ * documentMaxScrollX() // -> document.scrollingElement.scrollWidth - document.scrollingElement.clientWidth
+ * 
+ * const scrollingElement = document.createElement('div')
+ * 
+ * const scrollingElementMaxScrollX = maxScrollX(scrollingElement)
+ * 
+ * scrollingElementMaxScrollX() // -> scrollingElement.scrollWidth - scrollingElement.clientWidth
+ * ```
+ * 
  * @returns a function that returns the maximum scroll position of the source argument in X axis
  */
 export function maxScrollX (
