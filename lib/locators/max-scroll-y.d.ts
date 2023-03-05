a1a45171d7ca00a44e1fdfadf371ccef361f626a
a.ladan <arvinall021@gmail.com>
2023-03-05 18:17:38 +0330

    docs(locators): add example to max-scroll-y documentation

--- lib/locators/max-scroll-y.d.ts
+++ lib/locators/max-scroll-y.d.ts
@@ -3,6 +3,19 @@ import { IUnitValue } from '../units'
 /**
  * @param source default value is `globalThis`
  * 
+ * @example
+ * ```js
+ * const documentMaxScrollY = maxScrollY()
+ * 
+ * documentMaxScrollY() // -> document.scrollingElement.scrollHeight - document.scrollingElement.clientHeight
+ * 
+ * const scrollingElement = document.createElement('div')
+ * 
+ * const scrollingElementMaxScrollY = maxScrollY(scrollingElement)
+ * 
+ * scrollingElementMaxScrollY() // -> scrollingElement.scrollHeight - scrollingElement.clientHeight
+ * ```
+ * 
  * @returns a function that returns the maximum scroll position of the source argument in Y axis
  */
 export function maxScrollY (
