353aa012bd7d40c3db642852a859c1c32142b26b
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-25 08:33:43 +0000

    docs(configs): add description to clean-edges
    add import statement to example
    make links native
    add category

--- lib/configs/clean-edges.d.ts
+++ lib/configs/clean-edges.d.ts
@@ -1,11 +1,35 @@
+import { listen } from '../listen'
+import { fromY as from } from './from-y'
+import { toY as to } from './to-y'
+import { uncleanEdges } from './unclean-edges'
+import { config } from './config'
+
 /**
+ * `cleanEdges` config tells {@link listen} to cover the edges
+ * when scroll is not in range, in both axis
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is behind `100` for example `50`,
+ *   and then it's come to beyond `300` for example `350`,
+ *   {@link listen} emits `300`
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is beyond `300` for example `350`,
+ *   and then it's come to behind `100` for example `50`,
+ *   {@link listen} emits `0`
+ * 
  * @example
  * ```js
+ * import { cleanEdges } from 'toomar'
+ * 
  * cleanEdges() // -> { cleanEdges: true }
  * ```
  * 
  * @returns an object with `cleanEdges` property that is equal to `true`
  * 
- * @see {@link configs/unclean-edges.uncleanEdges}
+ * @see {@link uncleanEdges}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function cleanEdges (): { cleanEdges: true }
