750b2d2b70afd6e3db89e0736ce303d10fc6b7f7
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-25 09:07:21 +0000

    docs(configs): add description to cover-edges
    add import statement to example
    make links native
    add category

--- lib/configs/cover-edges.d.ts
+++ lib/configs/cover-edges.d.ts
@@ -1,11 +1,35 @@
+import { listen } from '../listen'
+import { fromY as from } from './from-y'
+import { toY as to } from './to-y'
+import { uncoverEdges } from './uncover-edges'
+import { config } from './config'
+
 /**
+ * `coverEdges` config tells {@link listen} to cover the edges
+ * when scroll is in range, in both axis
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is beyond `100` and behind `300` for example `150`,
+ *   and then it's come to beyond `300` for example `350`,
+ *   {@link listen} emits `300`
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is beyond `100` and behind `300` for example `250`,
+ *   and then it's come to behind `100` for example `50`,
+ *   {@link listen} emits `0`
+ * 
  * @example
  * ```js
+ * import { coverEdges } from 'toomar'
+ * 
  * coverEdges() // -> { coverEdges: true }
  * ```
  * 
  * @returns an object with `coverEdges` property that is equal to `true`
  * 
- * @see {@link configs/uncover-edges.uncoverEdges}
+ * @see {@link uncoverEdges}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function coverEdges (): { coverEdges: true }
