a10493053fcda019dd44957c1b7033404bf4b523
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-25 09:49:15 +0000

    docs(configs): add description to from-y
    add import statement to example
    add category

--- lib/configs/from-y.d.ts
+++ lib/configs/from-y.d.ts
@@ -1,11 +1,24 @@
+import { listen } from '../listen'
+import { config } from './config'
+import { toY } from './to-y'
+import { fromX } from './from-x'
+import { toX } from './to-x'
 import { IUnitInput, IUnitValue } from '../units'
 
 /**
+ * `fromY` config, define {@link listen}'s start point in **Y** axis
+ * by {@link IUnitInput}
+ * 
  * @example
  * ```js
- * const viewPortSize = percent(() => window.innerHeight)
+ * import { percent, fromY } from 'toomar'
  * 
- * fromY(viewPortSize(150)) // -> { fromY: viewPortSize(150), from: viewPortSize(150) }
+ * const viewportHeight = percent(() => window.innerHeight)
+ * 
+ * fromY(viewportHeight(150)) // -> {
+ * //  fromY: viewportHeight(150),
+ * //  from: viewportHeight(150)
+ * // }
  * 
  * fromY(500) // -> { fromY: px(500), from: px(500) }
  * ```
@@ -13,5 +26,12 @@ import { IUnitInput, IUnitValue } from '../units'
  * @returns
  * an object with `fromY` and `from` properties
  * that both of them are equal to `value` argument
+ * 
+ * @see {@link toY}
+ * @see {@link fromX}
+ * @see {@link toX}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function fromY (value: IUnitInput): { fromY: IUnitValue, from: IUnitValue }
