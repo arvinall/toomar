b83f645a801b46b884d98c53573ed9d224f6d96f
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-25 09:48:34 +0000

    docs(configs): add description to from-x
    add import statement to example
    add category

--- lib/configs/from-x.d.ts
+++ lib/configs/from-x.d.ts
@@ -1,15 +1,32 @@
+import { listen } from '../listen'
+import { config } from './config'
+import { toX } from './to-x'
+import { fromY } from './from-y'
+import { toY } from './to-y'
 import { IUnitInput, IUnitValue } from '../units'
 
 /**
+ * `fromX` config, define {@link listen}'s start point in **X** axis
+ * by {@link IUnitInput}
+ * 
  * @example
  * ```js
- * const viewPortWidth = percent(() => window.innerWidth)
+ * import { percent, fromX } from 'toomar'
  * 
- * fromX(viewPortWidth(150)) // -> { fromX: viewPortWidth(150) }
+ * const viewportWidth = percent(() => window.innerWidth)
+ * 
+ * fromX(viewportWidth(150)) // -> { fromX: viewportWidth(150) }
  * 
  * fromX(500) // -> { fromX: px(500) }
  * ```
  * 
  * @returns an object with `fromX` property that is equal to `value` argument
+ * 
+ * @see {@link toX}
+ * @see {@link fromY}
+ * @see {@link toY}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function fromX (value: IUnitInput): { fromX: IUnitValue }
