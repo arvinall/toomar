e90871d2160bb7cbc3f77070ffc8d485f5bf0f3e
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-26 09:51:27 +0000

    docs(configs): add to-x description
    add import statement to example
    add category

--- lib/configs/to-x.d.ts
+++ lib/configs/to-x.d.ts
@@ -1,8 +1,18 @@
+import { listen } from '../listen'
+import { config } from './config'
+import { fromX } from './from-x'
+import { toY } from './to-y'
+import { fromY } from './from-y'
 import { IUnitInput, IUnitValue } from '../units'
 
 /**
+ * `toX` config, defines {@link listen}'s destination point in **X** axis
+ * by {@link IUnitInput}
+ * 
  * @example
  * ```js
+ * import { percent, toX } from 'toomar'
+ * 
  * const viewPortWidth = percent(() => window.innerWidth)
  * 
  * toX(viewPortWidth(150)) // -> { toX: viewPortWidth(150) }
@@ -11,5 +21,12 @@ import { IUnitInput, IUnitValue } from '../units'
  * ```
  * 
  * @returns an object with `toX` property that is equal to `value` argument
+ * 
+ * @see {@link fromX}
+ * @see {@link toY}
+ * @see {@link fromY}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function toX (value: IUnitInput): { toX: IUnitValue }
