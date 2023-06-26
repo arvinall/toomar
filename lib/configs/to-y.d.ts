7b122dd3bb81aef4f56de16f31e01b85ee13e88b
Arvin Ladan <arvinall@users.noreply.github.com>
2023-06-26 10:54:14 +0000

    docs(configs): add to-y description
    add import statement to example
    add category

--- lib/configs/to-y.d.ts
+++ lib/configs/to-y.d.ts
@@ -1,8 +1,18 @@
+import { listen } from '../listen'
+import { config } from './config'
+import { fromY } from './from-y'
+import { toX } from './to-x'
+import { fromX } from './from-x'
 import { IUnitInput, IUnitValue } from '../units'
 
 /**
+ * `toY` config, defines {@link listen}'s destination point in **Y** axis
+ * by {@link IUnitInput}
+ * 
  * @example
  * ```js
+ * import { percent, toY } from 'toomar'
+ * 
  * const viewPortSize = percent(() => window.innerHeight)
  * 
  * toY(viewPortSize(150)) // -> { toY: viewPortSize(150), to: viewPortSize(150) }
@@ -13,5 +23,12 @@ import { IUnitInput, IUnitValue } from '../units'
  * @returns
  * an object with `toY` and `to` properties
  * that both of them are equal to `value` argument
+ * 
+ * @see {@link fromY}
+ * @see {@link toX}
+ * @see {@link fromX}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function toY (value: IUnitInput): { toY: IUnitValue, to: IUnitValue }
