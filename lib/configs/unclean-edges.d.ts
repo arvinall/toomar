f54075d4f3e77500da0227730edac64054fa8fc8
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-01 08:16:09 +0000

    docs(configs): add unclean-edges description
    add import statement to example
    make links native
    add category

--- lib/configs/unclean-edges.d.ts
+++ lib/configs/unclean-edges.d.ts
@@ -1,11 +1,35 @@
+import { listen } from '../listen'
+import { fromY as from } from './from-y'
+import { toY as to } from './to-y'
+import { cleanEdges } from './clean-edges'
+import { config } from './config'
+
 /**
+ * `uncleanEdges` config tells {@link listen} to not cover the edges
+ * when scroll is not in range, in both axis
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is behind `100` for example `50`,
+ *   and then it's come to beyond `300` for example `350`,
+ *   {@link listen} doesn't emit because scroll is not in range
+ * 
+ * > When {@link from} is `100` and {@link to} is `300`,
+ *   and scroll is beyond `300` for example `350`,
+ *   and then it's come to behind `100` for example `50`,
+ *   {@link listen} doesn't emit because scroll is not in range
+ * 
  * @example
  * ```js
+ * import { uncleanEdges } from 'toomar'
+ * 
  * uncleanEdges() // -> { cleanEdges: false }
  * ```
  * 
  * @returns an object with `cleanEdges` property that is equal to `false`
  * 
- * @see {@link configs/clean-edges.cleanEdges}
+ * @see {@link cleanEdges}
+ * @see {@link config}
+ * 
+ * @category Configs
  */
 export function uncleanEdges (): { cleanEdges: false }
