cf3837a193199ccad10c07011f1d38d32e605c65
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-02 10:27:37 +0000

    docs(internals): add get-target-element categories

--- lib/internals/get-target-element.d.ts
+++ lib/internals/get-target-element.d.ts
@@ -1,14 +1,26 @@
 import { ITarget, IEventPresenter } from '../configs/scroll'
 
-/** @returns `_document?.scrollingElement` */
+/**
+ * @returns `_document?.scrollingElement`
+ * 
+ * @category Internals
+ */
 export function getIfHasScrollingElement (_document): HTMLElement
 
-/** @returns `target?.document?.scrollingElement` */
+/**
+ * @returns `target?.document?.scrollingElement`
+ * 
+ * @category Internals
+ */
 export function getIfHasScrollingElementIfHasDocument (
   target: ITarget | EventTarget
 ): ReturnType<typeof getIfHasScrollingElement>
 
-/** @returns real HTMLElement target from events */
+/**
+ * @returns real HTMLElement target from events
+ * 
+ * @category Internals
+ */
 export function getTargetElement (
   event: IEventPresenter<unknown, unknown> | Event
 ): ReturnType<typeof getIfHasScrollingElementIfHasDocument>
