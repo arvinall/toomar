5478f781dc55e0a79f1ec9b655374449c5793635
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-04 14:32:15 +0000

    docs(locators): add bottom-of description
    add import statement to examples
    add category

--- lib/locators/bottom-of.d.ts
+++ lib/locators/bottom-of.d.ts
@@ -1,26 +1,53 @@
+import { rightOf } from './right-of'
+import { topOf } from './top-of'
+import { rightOf } from './right-of'
 import { IUnitValue } from '../units'
 
 /**
- * @param viewport default value is `globalThis`
+ * `bottomOf` used to locate bottom position of DOM element
+ * relative to the viewport or another element
  * 
- * @example
+ * @example Relative to the document viewport
  * ```js
+ * import { bottomOf } from 'toomar'
+ * 
  * const section1Element = document.querySelector('.section--1')
  * 
  * const bottomOfSection1Element = bottomOf(section1Element)
  * 
- * bottomOfSection1Element() // -> section1Element.getBoundingClientRect().bottom + document.scrollingElement.scrollTop
+ * bottomOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().bottom +
+ * //    document.scrollingElement.scrollTop
+ * // )
+ * ```
+ * 
+ * @example Relative to the DOM element
+ * ```js
+ * import { bottomOf } from 'toomar'
+ * 
+ * const section1Element = document.querySelector('.section--1')
  * 
  * const appElement = document.querySelector('.app')
  * 
  * const bottomOfAppSection1Element = bottomOf(section1Element, appElement)
  * 
- * bottomOfSection1Element() // -> section1Element.getBoundingClientRect().bottom + appElement.scrollTop
+ * bottomOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().bottom + appElement.scrollTop
+ * // )
  * ```
  * 
- * @returns a function that returns bottom of the target element position relative to viewport argument
+ * @returns a function that returns bottom position of the target element relative to the viewport argument
+ * 
+ * @see {@link rightOf}
+ * @see {@link topOf}
+ * @see {@link rightOf}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element Element}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis globalThis}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Document Document}
+ * 
+ * @category Locators
  */
 export function bottomOf (
   target: Element,
-  viewport?: typeof globalThis | Document | Element
+  viewport?: typeof globalThis | Document | Element = globalThis
 ): IUnitValue
