4de8c658360e7dab866e1053d7210a2308357af1
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-04 14:44:35 +0000

    docs(locators): add max-scroll-x description
    add import statement to examples
    add category

--- lib/locators/max-scroll-x.d.ts
+++ lib/locators/max-scroll-x.d.ts
@@ -1,23 +1,44 @@
+import { maxScrollY } from './max-scroll-y'
 import { IUnitValue } from '../units'
 
 /**
- * @param source default value is `globalThis`
+ * `maxScrollX` used to locate maximum scroll position in **X** axis
+ * based on the viewport or another element
  * 
- * @example
+ * @example Based on the document viewport
  * ```js
+ * import { maxScrollX } from 'toomar'
+ * 
  * const documentMaxScrollX = maxScrollX()
  * 
- * documentMaxScrollX() // -> document.scrollingElement.scrollWidth - document.scrollingElement.clientWidth
+ * documentMaxScrollX() // -> (
+ * //  document.scrollingElement.scrollWidth -
+ * //    document.scrollingElement.clientWidth
+ * // )
+ * ```
+ * 
+ * @example Based on the DOM element
+ * ```js
+ * import { maxScrollX } from 'toomar'
  * 
  * const scrollingElement = document.createElement('div')
  * 
  * const scrollingElementMaxScrollX = maxScrollX(scrollingElement)
  * 
- * scrollingElementMaxScrollX() // -> scrollingElement.scrollWidth - scrollingElement.clientWidth
+ * scrollingElementMaxScrollX() // -> (
+ * //  scrollingElement.scrollWidth - scrollingElement.clientWidth
+ * // )
  * ```
  * 
  * @returns a function that returns the maximum scroll position of the source argument in X axis
+ * 
+ * @see {@link maxScrollY}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element Element}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis globalThis}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Document Document}
+ * 
+ * @category Locators
  */
 export function maxScrollX (
-  source?: typeof globalThis | Document | Element
+  source?: typeof globalThis | Document | Element = globalThis
 ): IUnitValue
