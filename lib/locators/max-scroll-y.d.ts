73cf28d1422e262315a5710437e94dca1aa062c2
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-04 14:52:02 +0000

    docs(locators): add max-scroll-y description
    add import statement to examples
    add category

--- lib/locators/max-scroll-y.d.ts
+++ lib/locators/max-scroll-y.d.ts
@@ -1,23 +1,51 @@
+import { maxScrollX } from './max-scroll-x'
 import { IUnitValue } from '../units'
 
 /**
- * @param source default value is `globalThis`
+ * `maxScrollY` used to locate maximum scroll position in **Y** axis
+ * based on the viewport or another element
  * 
- * @example
+ * @example Based on the document viewport
  * ```js
+ * import { maxScrollY } from 'toomar'
+ * 
  * const documentMaxScrollY = maxScrollY()
  * 
- * documentMaxScrollY() // -> document.scrollingElement.scrollHeight - document.scrollingElement.clientHeight
+ * documentMaxScrollY() // -> (
+ * //  document.scrollingElement.scrollHeight -
+ * //    document.scrollingElement.clientHeight
+ * // )
+ * ```
+ * 
+ * @example Based on the DOM element
+ * ```js
+ * import { maxScrollY } from 'toomar'
  * 
  * const scrollingElement = document.createElement('div')
  * 
  * const scrollingElementMaxScrollY = maxScrollY(scrollingElement)
  * 
- * scrollingElementMaxScrollY() // -> scrollingElement.scrollHeight - scrollingElement.clientHeight
+ * scrollingElementMaxScrollY() // -> (
+ * //  scrollingElement.scrollHeight - scrollingElement.clientHeight
+ * // )
  * ```
  * 
  * @returns a function that returns the maximum scroll position of the source argument in Y axis
+ * 
+ * @see {@link maxScrollX}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element Element}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis globalThis}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Document Document}
+ * 
+ * @category Locators
  */
 export function maxScrollY (
-  source?: typeof globalThis | Document | Element
+  source?: typeof globalThis | Document | Element = globalThis
 ): IUnitValue
+
+/**
+ * ### Alias for {@link maxScrollY}
+ * 
+ * @category Locators
+ */
+export const maxScroll = maxScrollY
