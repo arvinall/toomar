afb358662096808287b8104fc50f117bd2e9f441
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-05 12:24:19 +0000

    docs(locators): add top-of description
    add import statement to examples
    add category

--- lib/locators/top-of.d.ts
+++ lib/locators/top-of.d.ts
@@ -1,26 +1,43 @@
 import { IUnitValue } from '../units'
 
 /**
- * @param viewport default value is `globalThis`
+ * `topOf` used to locate top position of DOM element
+ * relative to the viewport or another element
  * 
- * @example
+ * @example Relative to the document viewport
  * ```js
+ * import { topOf } from 'toomar'
+ * 
  * const section1Element = document.querySelector('.section--1')
  * 
  * const topOfSection1Element = topOf(section1Element)
  * 
- * topOfSection1Element() // -> section1Element.getBoundingClientRect().top + document.scrollingElement.scrollTop
+ * topOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().top +
+ * //    document.scrollingElement.scrollTop
+ * // )
+ * ```
+ * 
+ * @example Relative to the DOM element
+ * ```js
+ * import { topOf } from 'toomar'
+ * 
+ * const section1Element = document.querySelector('.section--1')
  * 
  * const appElement = document.querySelector('.app')
  * 
  * const topOfAppSection1Element = topOf(section1Element, appElement)
  * 
- * topOfSection1Element() // -> section1Element.getBoundingClientRect().top + appElement.scrollTop
+ * topOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().top + appElement.scrollTop
+ * // )
  * ```
  * 
- * @returns a function that returns top of the target element position relative to viewport argument
+ * @returns a function that returns top position of the target element relative to the viewport argument
+ * 
+ * @category Locators
  */
 export function topOf (
   target: Element,
-  viewport?: typeof globalThis | Document | Element
+  viewport?: typeof globalThis | Document | Element = globalThis
 ): IUnitValue
