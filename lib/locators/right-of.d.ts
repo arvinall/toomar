0e52606a1f48bdf5bcb3e9bc342643243da68e6c
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-05 12:20:36 +0000

    docs(locators): add right-of description
    add import statement to examples
    add category

--- lib/locators/right-of.d.ts
+++ lib/locators/right-of.d.ts
@@ -1,26 +1,43 @@
 import { IUnitValue } from '../units'
 
 /**
- * @param viewport default value is `globalThis`
+ * `rightOf` used to locate right position of DOM element
+ * relative to the viewport or another element
  * 
- * @example
+ * @example Relative to the document viewport
  * ```js
+ * import { rightOf } from 'toomar'
+ * 
  * const section1Element = document.querySelector('.section--1')
  * 
  * const rightOfSection1Element = rightOf(section1Element)
  * 
- * rightOfSection1Element() // -> section1Element.getBoundingClientRect().right + document.scrollingElement.scrollLeft
+ * rightOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().right +
+ * //    document.scrollingElement.scrollLeft
+ * // )
+ * ```
+ * 
+ * @example Relative to the DOM element
+ * ```js
+ * import { rightOf } from 'toomar'
+ * 
+ * const section1Element = document.querySelector('.section--1')
  * 
  * const appElement = document.querySelector('.app')
  * 
  * const rightOfAppSection1Element = rightOf(section1Element, appElement)
  * 
- * rightOfSection1Element() // -> section1Element.getBoundingClientRect().right + appElement.scrollLeft
+ * rightOfSection1Element() // -> (
+ * //  section1Element.getBoundingClientRect().right + appElement.scrollLeft
+ * // )
  * ```
  * 
- * @returns a function that returns right of the target element position relative to viewport argument
+ * @returns a function that returns right position of the target element relative to the viewport argument
+ * 
+ * @category Locators
  */
 export function rightOf (
   target: Element,
-  viewport?: typeof globalThis | Document | Element
+  viewport?: typeof globalThis | Document | Element = globalThis
 ): IUnitValue
