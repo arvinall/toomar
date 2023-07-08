6e642a912d7a54ab1b86215504738f47f037e4a8
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-08 13:16:54 +0000

    docs(schedulers): add auditAnimationFrame description
    add import statement to example
    add category

--- lib/schedulers/audit-animation-frame.d.ts
+++ lib/schedulers/audit-animation-frame.d.ts
@@ -1,21 +1,34 @@
 import { MonoTypeOperatorFunction as RxMonoTypeOperatorFunction } from 'rxjs'
 
+import { listen } from '../listen'
+
 /**
+ * `auditAnimationFrame` scheduler
+ * runs your `rxjs.Subscriber` in `requestAnimationFrame`
+ * 
  * @example
  * ```js
+ * import { to, config, listen, auditAnimationFrame } from 'toomar'
+ * 
  * listen(config(to(500)))
  *   .pipe(
- *     auditAnimationFrame() // -> rxAuditTime(0, rxAnimationFrameScheduler)
+ *     auditAnimationFrame() // -> rx.auditTime(0, rx.animationFrameScheduler)
  *   )
- *   .subscribe(({ y }) => { // Do heavy animation operations with GPU acceleration })
+ *   .subscribe(({ y }) => {
+ *     // Do heavy animation operations with GPU acceleration (when available)
+ *   })
  * ```
  * 
  * @returns
- * combination of [rxjs.animationFrameScheduler](https://rxjs.dev/api/index/const/animationFrameScheduler)
- * and [rxjs.auditTime](https://rxjs.dev/api/index/function/auditTime) with duration `0`
- * monotype operator function
+ * combination of `rxjs.animationFrameScheduler` and `rxjs.auditTime`
+ * with duration `0`, monotype operator function
  * 
+ * @see listen
+ * @see [requestAnimationFrame](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)
+ * @see [rxjs.Subscriber](https://rxjs.dev/api/index/class/Subscriber)
  * @see [rxjs.animationFrameScheduler](https://rxjs.dev/api/index/const/animationFrameScheduler)
  * @see [rxjs.auditTime](https://rxjs.dev/api/index/function/auditTime)
+ * 
+ * @category Schedulers
  */
 export function auditAnimationFrame <T> (): RxMonoTypeOperatorFunction<T>
