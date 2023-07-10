9c83f4ca05efd2f3ae42d0628900f080a6d3ff4f
Arvin Ladan <arvinall@users.noreply.github.com>
2023-07-10 14:36:32 +0000

    docs(units): add units types descriptions
    add category

--- lib/units/index.d.ts
+++ lib/units/index.d.ts
@@ -1,11 +1,52 @@
-/** UnitValue is a function that returns a number */
+import { listen } from '../listen'
+import { from } from '../configs/from-y'
+import { fromX } from '../configs/from-x'
+import { to } from '../configs/to-y'
+import { toX } from '../configs/to-x'
+
+/**
+ * `unitValue` is a function that returns a `number`,
+ * used as {@link IUnit}'s returned value,
+ * or used as {@link IUnit}'s arguments
+ * when you want to compute position on each iteration
+ * 
+ * @see {@link listen}
+ * @see {@link from}
+ * @see {@link to}
+ * 
+ * @category Units
+ */
 export interface IUnitValue { (): number }
 
-/** Unit is a function that returns a {@link IUnitValue} */
-export interface IUnit { (...args: unknown[]): IUnitValue }
-
-/** UnitInput is a number or {@link IUnitValue} */
+/**
+ * `unitInput` is a `number` or a {@link IUnitValue}
+ * (function that returns `number`),
+ * that used as {@link IUnit}'s arguments
+ * 
+ * @see {@link listen}
+ * @see {@link from}
+ * @see {@link to}
+ * 
+ * @category Units
+ */
 export type IUnitInput = number | IUnitValue
+
+/**
+ * `unit` is a function that accepts one or more {@link IUnitInput}s
+ * and returns a {@link IUnitValue}
+ * 
+ * units are used to specify beginning and end of {@link listen}'s edges
+ * via {@link from}/{@link fromX} and {@link to}/{@link toX}
+ * 
+ * @see {@link listen}
+ * @see {@link from}
+ * @see {@link fromX}
+ * @see {@link to}
+ * @see {@link toX}
+ * 
+ * @category Units
+ */
+export interface IUnit { (...args: IUnitInput[]): IUnitValue }
 
 export { px } from './px'
 
