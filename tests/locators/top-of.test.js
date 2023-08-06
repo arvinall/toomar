958e95d87cb429c21d13b8b754b4cb13593d0f0d
Arvin Ladan <arvinall@users.noreply.github.com>
2023-08-06 11:59:34 +0000

    test(locators): add tests to pass funcs to top-of

--- tests/locators/top-of.test.js
+++ tests/locators/top-of.test.js
@@ -46,7 +46,7 @@ test(
 
 test(
   'topOf must use globalThis as default value for viewport argument' + ' ' +
-  'and return a function' + ' ' +
+  'and returns a function' + ' ' +
   'that returns the top of the target position relative to the viewport argument',
   () => {
     const target = { getBoundingClientRect: jest.fn() }
@@ -59,14 +59,77 @@ test(
       const scrollingElement = { get scrollTop () { return scrollTop } }
 
       const assigner = [
-        () => Object.assign(globalThis, scrollingElement),
-        () => Object.assign(globalThis, { scrollingElement }),
-        () => Object.assign(globalThis, { document: { scrollingElement } })
+        () => {
+          Object.assign(globalThis, scrollingElement)
+
+          return () => delete globalThis.scrollTop
+        },
+        () => {
+          Object.assign(globalThis, { scrollingElement })
+
+          return () => delete globalThis.scrollingElement
+        },
+        () => {
+          Object.assign(globalThis, { document: { scrollingElement } })
+
+          return () => delete globalThis.document
+        }
+      ][idx]
+
+      let cleanup = assigner()
+
+      const topOfViewportTarget = topOf(target)
+
+      target.getBoundingClientRect.mockReturnValue({ top: 20 })
+
+      expect(topOfViewportTarget())
+        .toBe(target.getBoundingClientRect().top + scrollTop)
+
+      scrollTop = 15
+
+      if (!idx) {
+        cleanup()
+        cleanup = assigner()
+      }
+
+      target.getBoundingClientRect.mockReturnValue({ top: 35 })
+
+      expect(topOfViewportTarget())
+        .toBe(target.getBoundingClientRect().top + scrollTop)
+
+      cleanup()
+    }
+  }
+)
+
+test(
+  'topOf must return a function' + ' ' +
+  'that returns the top of the target position relative to the viewport argument,' + ' ' +
+  'which its target or viewport are functions',
+  () => {
+    const target = { getBoundingClientRect: jest.fn() }
+
+    for (let idx in Array(3).fill()) {
+      idx = Number(idx)
+
+      const viewport = {}
+
+      let scrollTop = 10
+
+      const scrollingElement = { get scrollTop () { return scrollTop } }
+
+      const assigner = [
+        () => Object.assign(viewport, scrollingElement),
+        () => Object.assign(viewport, { scrollingElement }),
+        () => Object.assign(viewport, { document: { scrollingElement } })
       ][idx]
 
       assigner()
 
-      const topOfViewportTarget = topOf(target)
+      const topOfViewportTarget = [
+        topOf(() => target, viewport),
+        topOf(() => target, () => viewport)
+      ][Number(!!(idx % 2))]
 
       target.getBoundingClientRect.mockReturnValue({ top: 20 })
 
@@ -79,6 +142,65 @@ test(
       target.getBoundingClientRect.mockReturnValue({ top: 35 })
 
       expect(topOfViewportTarget()).toBe(target.getBoundingClientRect().top + scrollTop)
+    }
+  }
+)
+
+test(
+  'topOf must use globalThis as default value for viewport argument' + ' ' +
+  'and returns a function' + ' ' +
+  'that returns the top of the target position relative to the viewport argument' + ' ' +
+  'which its target is a function',
+  () => {
+    const target = { getBoundingClientRect: jest.fn() }
+
+    for (let idx in Array(3).fill()) {
+      idx = Number(idx)
+
+      let scrollTop = 10
+
+      const scrollingElement = { get scrollTop () { return scrollTop } }
+
+      const assigner = [
+        () => {
+          Object.assign(globalThis, scrollingElement)
+
+          return () => delete globalThis.scrollTop
+        },
+        () => {
+          Object.assign(globalThis, { scrollingElement })
+
+          return () => delete globalThis.scrollingElement
+        },
+        () => {
+          Object.assign(globalThis, { document: { scrollingElement } })
+
+          return () => delete globalThis.document
+        }
+      ][idx]
+
+      let cleanup = assigner()
+
+      const topOfViewportTarget = topOf(() => target)
+
+      target.getBoundingClientRect.mockReturnValue({ top: 20 })
+
+      expect(topOfViewportTarget())
+        .toBe(target.getBoundingClientRect().top + scrollTop)
+
+      scrollTop = 15
+
+      if (!idx) {
+        cleanup()
+        cleanup = assigner()
+      }
+
+      target.getBoundingClientRect.mockReturnValue({ top: 35 })
+
+      expect(topOfViewportTarget())
+        .toBe(target.getBoundingClientRect().top + scrollTop)
+
+      cleanup()
     }
   }
 )
