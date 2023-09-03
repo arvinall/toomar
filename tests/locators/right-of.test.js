b36995c9c6785f8de21d208b88ee375fe4602766
Arvin Ladan <arvinall@users.noreply.github.com>
2023-09-03 08:49:37 +0000

    test(locators): add tests to pass funcs to right-of

--- tests/locators/right-of.test.js
+++ tests/locators/right-of.test.js
@@ -46,7 +46,7 @@ test(
 
 test(
   'rightOf must use globalThis as default value for viewport argument' + ' ' +
-  'and return a function' + ' ' +
+  'and returns a function' + ' ' +
   'that returns the right of the target position relative to the viewport argument',
   () => {
     const target = { getBoundingClientRect: jest.fn() }
@@ -59,18 +59,82 @@ test(
       const scrollingElement = { get scrollLeft () { return scrollLeft } }
 
       const assigner = [
-        () => Object.assign(globalThis, scrollingElement),
-        () => Object.assign(globalThis, { scrollingElement }),
-        () => Object.assign(globalThis, { document: { scrollingElement } })
+        () => {
+          Object.assign(globalThis, scrollingElement)
+
+          return () => delete globalThis.scrollTop
+        },
+        () => {
+          Object.assign(globalThis, { scrollingElement })
+
+          return () => delete globalThis.scrollingElement
+        },
+        () => {
+          Object.assign(globalThis, { document: { scrollingElement } })
+
+          return () => delete globalThis.document
+        }
       ][idx]
 
-      assigner()
+      let cleanup = assigner()
 
       const rightOfViewportTarget = rightOf(target)
 
       target.getBoundingClientRect.mockReturnValue({ right: 20 })
 
-      expect(rightOfViewportTarget()).toBe(target.getBoundingClientRect().right + scrollLeft)
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
+
+      scrollLeft = 15
+
+      if (!idx) {
+        cleanup()
+        cleanup = assigner()
+      }
+
+      target.getBoundingClientRect.mockReturnValue({ right: 35 })
+
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
+
+      cleanup()
+    }
+  }
+)
+
+test(
+  'rightOf must return a function' + ' ' +
+  'that returns the right of the target position relative to the viewport argument,' + ' ' +
+  'which its target or viewport are functions',
+  () => {
+    const target = { getBoundingClientRect: jest.fn() }
+
+    for (let idx in Array(3).fill()) {
+      idx = Number(idx)
+
+      const viewport = {}
+
+      let scrollLeft = 10
+
+      const scrollingElement = { get scrollLeft () { return scrollLeft } }
+
+      const assigner = [
+        () => Object.assign(viewport, scrollingElement),
+        () => Object.assign(viewport, { scrollingElement }),
+        () => Object.assign(viewport, { document: { scrollingElement } })
+      ][idx]
+
+      assigner()
+
+      const rightOfViewportTarget = [
+        rightOf(() => target, viewport),
+        rightOf(() => target, () => viewport)
+      ][Number(!!(idx % 2))]
+
+      target.getBoundingClientRect.mockReturnValue({ right: 20 })
+
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
 
       scrollLeft = 15
 
@@ -78,7 +142,67 @@ test(
 
       target.getBoundingClientRect.mockReturnValue({ right: 35 })
 
-      expect(rightOfViewportTarget()).toBe(target.getBoundingClientRect().right + scrollLeft)
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
+    }
+  }
+)
+
+test(
+  'rightOf must use globalThis as default value for viewport argument' + ' ' +
+  'and returns a function' + ' ' +
+  'that returns the right of the target position relative to the viewport argument' + ' ' +
+  'which its target is a function',
+  () => {
+    const target = { getBoundingClientRect: jest.fn() }
+
+    for (let idx in Array(3).fill()) {
+      idx = Number(idx)
+
+      let scrollLeft = 10
+
+      const scrollingElement = { get scrollLeft () { return scrollLeft } }
+
+      const assigner = [
+        () => {
+          Object.assign(globalThis, scrollingElement)
+
+          return () => delete globalThis.scrollTop
+        },
+        () => {
+          Object.assign(globalThis, { scrollingElement })
+
+          return () => delete globalThis.scrollingElement
+        },
+        () => {
+          Object.assign(globalThis, { document: { scrollingElement } })
+
+          return () => delete globalThis.document
+        }
+      ][idx]
+
+      let cleanup = assigner()
+
+      const rightOfViewportTarget = rightOf(() => target)
+
+      target.getBoundingClientRect.mockReturnValue({ right: 20 })
+
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
+
+      scrollLeft = 15
+
+      if (!idx) {
+        cleanup()
+        cleanup = assigner()
+      }
+
+      target.getBoundingClientRect.mockReturnValue({ right: 35 })
+
+      expect(rightOfViewportTarget())
+        .toBe(target.getBoundingClientRect().right + scrollLeft)
+
+      cleanup()
     }
   }
 )
